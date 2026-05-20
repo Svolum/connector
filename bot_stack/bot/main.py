da7119b812424889faa810e2c9acdaf126a1e7db
misha <mihailgavrilov118@gmail.com>
2026-05-20 13:30:01 +0800

    не знаю что добавил

--- bot_stack/bot/main.py
+++ bot_stack/bot/main.py
@@ -1,47 +1,68 @@
+import os
 import asyncio
 
 import uvicorn
 from fastapi import FastAPI, Body, Response
 from fastapi.responses import PlainTextResponse
 
-from telegram import Update
+import telegram
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
 
+import httpx
+
+import base64
+import io
+
 import tg_api
-import tg_handlers
-
-import load_env
 
 
-SELF_URL = load_env.SELF_URL
-WEB_URL: str = load_env.WEB_URL
-PORT = load_env.PORT
-TOKEN = load_env.TOKEN
-WEBHOO_PATH = load_env.WEBHOO_PATH
-PROXY_URL = load_env.PROXY_URL
+SELF_URL = os.getenv("SELF_URL", "")
+WEB_URL: str = os.getenv("WEB_URL", "")
+PORT = int(os.getenv("PORT", "8000"))
+TOKEN = os.getenv("TOKEN")
+WEBHOO_PATH = os.getenv("WEBHOO_PATH", "telegram")
+PROXY_URL = os.getenv("PROXY_URL", "NO")
+HTTPX_TIMEOUT = float(os.getenv("HTTPX_TIMEOUT", "30.0"))
+HEADER_NAME = os.getenv("HEADER_NAME", "X-Connector-Name")
+HEADER_VALUE = os.getenv("HEADER_VALUE", "tg")
+USE_HEADER = os.getenv("USE_HEADER", "YES")
+
+
+header = {
+    HEADER_NAME: HEADER_VALUE
+}
+if (USE_HEADER == "NO"):
+    header = dict()
+
+bot: telegram.Bot = None
+
 
 async def main() -> None:
-    # Создаем приложение бота
-    application = (
-        Application.builder()
-        .token(TOKEN)
-        .proxy(PROXY_URL)
-        .build()
-    )
-    # Регистрируем функции обработчики команд
-    application.add_handler(CommandHandler("start", tg_handlers.command_start))
-    application.add_handler(CommandHandler("help", tg_handlers.command_help))
-    application.add_handler(CallbackQueryHandler(tg_handlers.handle_button_callback))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, tg_handlers.handle_message_text))
-    application.add_handler(MessageHandler(filters.PHOTO, tg_handlers.handle_image))
+    if (PROXY_URL == "NO"):
+        application = (
+            Application.builder()
+            .token(TOKEN)
+            .build()
+        )
+    else:
+        application = (
+            Application.builder()
+            .token(TOKEN)
+            .proxy(PROXY_URL)
+            .build()
+        )
+    application.add_handler(MessageHandler(filters.COMMAND, handle_command))
+    application.add_handler(CallbackQueryHandler(handle_button_callback))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message_text))
+    application.add_handler(MessageHandler(filters.PHOTO, handle_image))
+
+    bot = application.bot
 
     # Настраиваем вебхук для Telegram
-    await application.bot.set_webhook(url=f"{SELF_URL}/{WEBHOO_PATH}", allowed_updates=Update.ALL_TYPES)
+    await bot.set_webhook(url=f"{SELF_URL}/{WEBHOO_PATH}", allowed_updates=Update.ALL_TYPES)
 
-    # Создаем приложение FastAPI, который интернет слушает
     tg_api.application = application
-    tg_handlers.bot = tg_api.application.bot
-    tg_handlers.WEB_URL = WEB_URL
 
 
     webserver = uvicorn.Server(
@@ -53,6 +74,105 @@ async def main() -> None:
         await application.start()
         await webserver.serve()
         await application.stop()
+
+
+async def handle_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # 1. Получаем полный текст (например, "/start", "/help" или "/z 123")
+    command = update.message.text
+    if not command:
+        return
+
+
+    command = command.replace('/', '')
+    print("команда от")
+    print(update.message.chat_id, command)
+
+    async with httpx.AsyncClient(trust_env=False, headers=header) as client:
+        payload = {
+                "user_id": update.message.from_user.id,
+                "place": {
+                    "chat_id": update.message.chat_id
+                },
+                "name": command,
+                "date_time": update.message.date.isoformat()
+        }
+        response = await client.post(url=f"{WEB_URL}/command", json=payload, timeout=HTTPX_TIMEOUT)
+
+
+async def handle_message_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print("ТЕКСТ от")
+    print(update.message.chat_id, update.message.text)
+    async with httpx.AsyncClient(trust_env=False, headers=header) as client:
+        payload = {
+                "user_id": update.message.from_user.id,
+                "place": {
+                    "chat_id": update.message.chat_id
+                },
+                "text": update.message.text,
+                "date_time": update.message.date.isoformat()
+        }
+        response = await client.post(url=f"{WEB_URL}/user_message", json=payload, timeout=HTTPX_TIMEOUT)
+
+
+async def handle_button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    # отвечаем на callback, чтобы у пользователя убралась анимация загрузки
+    await query.answer()
+
+    await query.edit_message_text(text=f'Selected option: {query.data}')
+
+    async with httpx.AsyncClient(trust_env=True, headers=header) as client:
+        payload = {
+            "user_id": str(query.from_user.id),
+            "button": query.data,
+            "place": {
+                "chat_id": str(query.message.chat.id),
+                "message_id": str(query.message.message_id)
+            },
+            "date_time": query.message.date.isoformat()
+        }
+
+        try:
+            response = await client.post(url=f"{WEB_URL}/keyboard/input", json=payload, timeout=HTTPX_TIMEOUT)
+            response.raise_for_status()
+        except Exception as e:
+            print(f"Ошибка при отправке в FastAPI: {e}")
+
+
+async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Берем самое качественное фото(последнее в списке)
+    photo = update.message.photo[-1]
+
+    # Получаем файл от Telegram
+    new_file = await context.bot.get_file(photo.file_id)
+
+    # Скачиваем в "виртуальный" файл в памяти
+    photo_buffer = io.BytesIO()
+    await new_file.download_to_memory(photo_buffer)
+
+    # Кодируем байты в base64 строку
+    # .getvalue() — получает все байты из буфера
+    # base64.b64encode — кодирует байты в base64-байты
+    # .decode('utf-8') — превращает байты base64 в обычную строку
+    image_base64 = base64.b64encode(photo_buffer.getvalue()).decode('utf-8')
+
+    payload = {
+        "user_id": str(update.effective_user.id),
+        "place": {
+            "chat_id": str(update.effective_chat.id)
+        },
+        "attachments_base64": [
+            image_base64
+        ],
+        "date_time": update.message.date.isoformat()
+    }
+
+    async with httpx.AsyncClient(trust_env=False, headers=header) as client:
+        response = await client.post(
+            f"{WEB_URL}/image",
+            json=payload,
+            timeout=HTTPX_TIMEOUT * 10
+        )
 
 
 if __name__ == "__main__":
--- bot_stack/bot/tg_api.py
+++ bot_stack/bot/tg_api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import PlainTextResponse, Response
 from starlette.responses import JSONResponse
@@ -12,7 +12,7 @@ import base64, io
 
 application: Application = None
 
-fast_api_app: FastAPI = FastAPI()
+fast_api_app: FastAPI = FastAPI(root_path="/tg_bot")
 
 # Эндпоинт для обычных сообщений из Telegram
 @fast_api_app.post("/telegram")
@@ -29,6 +29,8 @@ async def health() -> PlainTextResponse:
 
 @fast_api_app.post("/message")
 async def send_message_api(data: dict = Body()) -> JSONResponse:
+    print("/message")
+    print(data)
     try:
         chat_id = int(data["place"]["chat_id"])
         text = data["text"]
@@ -37,22 +39,31 @@ async def send_message_api(data: dict = Body()) -> JSONResponse:
                           "code": 0,
                           "message": "не передан chat_id"
         }
-        return JSONResponse(content=jsonable_encoder(message), status_code=400)
+        return JSONResponse(content=jsonable_encoder(message), status_code=500)
     try:
-        await application.bot.send_message(chat_id=chat_id, text=text)
+        sent_message = await application.bot.send_message(chat_id=chat_id, text=text)
     except Exception as e:
         message: dict = {
             "code": 0,
             "message": "не удалось отправить сообщение"
         }
-        return JSONResponse(content=jsonable_encoder(message), status_code=400)
+        return JSONResponse(content=jsonable_encoder(message), status_code=500)
 
-    return PlainTextResponse("Сообщение отправлено!", status_code=200)
+    return JSONResponse(content=jsonable_encoder({
+        "user_id": data["user_id"],
+        "place": {
+            "chat_id": str(chat_id),
+            "message_id": str(sent_message.message_id)
+        },
+        "date_time": sent_message.date.isoformat()
+    }), status_code=200)
 
 
 @fast_api_app.post("/keyboard/create")
 async def create_keyboard(data: dict = Body()) -> JSONResponse:
     keyboard: list = list()
+    print("/message")
+    print(data)
     try:
         for i in data["buttons"]:
             keyboard.append([InlineKeyboardButton(text=i["text"], callback_data=i["text"])])
@@ -63,7 +74,7 @@ async def create_keyboard(data: dict = Body()) -> JSONResponse:
                 "code": 0,
                 "message": "не переданы кнопки\n" + e.__str__(),
             }
-        ), status_code=400)
+        ), status_code=500)
     sent_message = await application.bot.send_message(
         chat_id=data["place"]["chat_id"],
         text=data["title"],
@@ -74,7 +85,7 @@ async def create_keyboard(data: dict = Body()) -> JSONResponse:
             "user_id": data["user_id"],
             "place": {
                 "chat_id": data["place"]["chat_id"],
-                "message_id": sent_message.message_id
+                "message_id": str(sent_message.message_id)
             },
             "date_time": sent_message.date.isoformat()
         }
@@ -83,6 +94,8 @@ async def create_keyboard(data: dict = Body()) -> JSONResponse:
 
 @fast_api_app.post("/keyboard/update")
 async def create_keyboard(data: dict = Body()) -> JSONResponse:
+    print("/message")
+    print(data)
     keyboard: list = list()
     try:
         for i in data["buttons"]:
@@ -94,23 +107,21 @@ async def create_keyboard(data: dict = Body()) -> JSONResponse:
                 "code": 0,
                 "message": "не переданы кнопки\n" + e.__str__(),
             }
-        ), status_code=400)
+        ), status_code=500)
     sent_message = await application.bot.edit_message_text(
         chat_id=data["place"]["place"]["chat_id"],
         message_id=data["place"]["place"]["message_id"],
         text=data["title"],
         reply_markup=reply_markup
     )
-    return JSONResponse(content=jsonable_encoder(
-        {
-            "user_id": data["user_id"],
-            "place": {
-                "chat_id": data["place"]["chat_id"],
-                "message_id": sent_message.message_id
-            },
-            "date_time": sent_message.date.isoformat()
-        }
-    ), status_code=200)
+    return JSONResponse(content=jsonable_encoder({
+        "user_id": data["user_id"],
+        "place": {
+            "chat_id": str(sent_message.chat.id),
+            "message_id": str(sent_message.message_id)
+        },
+        "date_time": sent_message.date.isoformat()
+    }), status_code=200)
 
 
 @fast_api_app.post("/image")
@@ -138,3 +149,12 @@ async def create_keyboard_with_photo(data: dict = Body()):
                 "date_time": sent_message.date.isoformat()
             }
         ), status_code=200)
+
+
+@fast_api_app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"])
+async def catch_all(request: Request, full_path: str):
+    print('стучатся не туда', request.url.path)
+    return JSONResponse(
+        status_code=404,
+        content={"message": f"Путь /{full_path} с методом {request.method} не существует."}
+    )
